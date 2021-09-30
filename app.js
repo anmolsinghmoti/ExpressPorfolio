045822308d4b78d32d8da5d52d0b336b0c50b383
Anmol Singh <anmolsinghmoti@gmail.com>
2021-09-30 17:32:25 -0400

    app.js modified

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.static(path_1.default.join(__dirname, 'client')));
 app.use(express_1.default.static(path_1.default.join(__dirname, 'node_modules')));
-app.use('/', index_1.default);
+//router middleware
+//app.use('/', index_1.default);
+app.use('/', indexRouter);
 app.use(function (req, res, next) {
     next((0, http_errors_1.default)(404));
 });
